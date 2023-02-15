83b1004b4f8e2add4cf38c97f48a1b7a59938ca4
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-02-15 11:06:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from crypt import methods
 import json
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo, ObjectId
-from flask_cors import CORS
+from flask_cors import CORS , cross_origin #ARREGLAR CORS
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost/pythonreactdb'
 mongo = PyMongo(app) #es la conexion para la base de datos
 
+CORS(app, support_credentials=True) #front end
+
+#colecciones
 db = mongo.db.users #coleccion #1
 
+
+#crear usuario
 @app.route('/users', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def createUser():
     id = db.insert_one({
         "name": request.json['name'],
@@ -19,7 +25,7 @@ def createUser():
     })
     return jsonify(str(ObjectId(id)))
 
-
+#listar TODOS los usuarios
 @app.route('/users', methods=['GET']) #obtener usuario
 def getUsers():
     users = []
@@ -34,7 +40,7 @@ def getUsers():
 
 
 
-
+#listar solo un usuario
 @app.route('/user/<id>', methods=['GET']) #solo uno
 def getUser(id):
     user = db.find_one({'_id': ObjectId(id)})
@@ -48,7 +54,7 @@ def getUser(id):
 
 
 
-
+#eliminar un usuario
 @app.route('/users/<id>', methods=['DELETE']) #delete user
 def deleteUser(id):
     db.delete_one({'_id': ObjectId(id)})
@@ -56,7 +62,7 @@ def deleteUser(id):
 
 
 
-
+#actualizar un usuario
 @app.route('/users/<id>', methods=['PUT']) #update use
 def updateUser(id):
     db.update_one({'_id': ObjectId(id)}, {'$set': {
